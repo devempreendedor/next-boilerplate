017c08aed9c74261a9219a66283b3727bf41cb2d
Lucas Couto <lucas@orbithub.com.br>
2021-12-04 16:07:54 -0300

    refactor: add Layout on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,18 @@
-import { ThemeToogle } from '@components/common'
+import { Layout } from '@components/common'
 import { Button } from '@components/ui'
 
 const HomePage = () => {
   return (
-    <main className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-semibold bg-pink-500 px-8 text-white">
-        Home Page
-      </h1>
-      <div className="mt-6">
-        <div className="flex item mb-4">
-          <p className="mr-4">Change theme:</p>
-          <ThemeToogle />
+    <Layout>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-semibold bg-pink-500 px-8 text-white">
+          Home Page
+        </h1>
+        <div className="mt-6">
+          <Button>Entrar</Button>
         </div>
-        <Button>Entrar</Button>
       </div>
-    </main>
+    </Layout>
   )
 }
 
