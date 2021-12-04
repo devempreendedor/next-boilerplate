e24bcd77252bdedc91b20d8457759f8439c0dc44
Lucas <lucas3couto@gmail.com>
2021-12-04 14:17:17 -0300

    refactor: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
-    '^@components(.*)$': '<rootDir>/components$1',
+    '^@components(.*)$': '<rootDir>/src/components$1',
     '^@pages(.*)$': '<rootDir>/src/pages$1',
     '^@styles(.*)$': '<rootDir>/src/styles$1',
     '^@hooks(.*)$': '<rootDir>/src/hooks$1',
