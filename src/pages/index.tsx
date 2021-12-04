88b0db6f2e4c0d843a2281c05aac70606c519b0f
Lucas Couto <lucas@orbithub.com.br>
2021-12-04 16:34:59 -0300

    refactor: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,18 @@ const HomePage = () => {
     <Layout>
       <div className="h-full flex flex-col items-center justify-center">
         <h1 className="text-6xl font-semibold bg-pink-500 px-8 text-white">
-          Home Page
+          NextJS Boilerplate
         </h1>
+        <p>A boilerplate built with NextJs, Typescript and TailwindCss</p>
         <div className="mt-6">
-          <Button>Entrar</Button>
+          <Button
+            onClick={() =>
+              (window.location.href =
+                'https://github.com/devempreendedor/next-boilerplate')
+            }
+          >
+            Access Repository
+          </Button>
         </div>
       </div>
     </Layout>
