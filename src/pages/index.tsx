50eb2d38e2f368fa5df912680c575a53050355d6
Lucas <lucas3couto@gmail.com>
2021-12-04 13:01:08 -0300

    refactor: add main on HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 const HomePage = () => {
   return (
-    <div className="h-screen bg-blue-400 flex items-center justify-center">
+    <main className="h-screen bg-blue-400 flex items-center justify-center">
       <h1 className="text-6xl font-semibold bg-white px-8">Home Page</h1>
-    </div>
+    </main>
   )
 }
 
