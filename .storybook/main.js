b6469fde78a765c03a1d4a78af487a7aae9baeb6
Lucas <lucas3couto@gmail.com>
2021-12-04 14:13:31 -0300

    refactor: absolute path on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,21 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules']
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@pages': path.resolve(__dirname, '../src/pages'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@styles': path.resolve(__dirname, '../src/styles'),
+      '@utils': path.resolve(__dirname, '../src/utils'),
+    }
+
+    return config
+  },
   framework: '@storybook/react',
 }
