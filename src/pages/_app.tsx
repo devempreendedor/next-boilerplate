72a23268aa97cff80ec4b8e68093d0f7c673338d
Lucas <lucas3couto@gmail.com>
2021-12-04 13:00:47 -0300

    refactor: _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import 'styles/main.css'
+import '@styles/main.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
