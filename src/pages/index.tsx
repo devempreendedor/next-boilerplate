3e42c5314e412c0906b6e0af7e2f485d517edb2e
Lucas <lucas3couto@gmail.com>
2021-12-04 12:42:39 -0300

    feat: add _app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 const HomePage = () => {
-  return <div>Home Page</div>
+  return (
+    <div className="h-screen bg-blue-400 flex items-center justify-center">
+      <h1 className="text-6xl font-semibold bg-white px-8">Home Page</h1>
+    </div>
+  )
 }
 
 export default HomePage
